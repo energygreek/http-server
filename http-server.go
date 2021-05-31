2319672abea8bb8deb0cdad8033a46a67523cf63
husongtao <husongtao@skybility.com>
2021-05-31 15:19:54 +0800

    use buildin FileServer

--- http-server.go
+++ http-server.go
@@ -12,47 +12,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"path/filepath"
 )
-
-var indexing_path string
-
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL)
-	file_path := filepath.Join(indexing_path, r.URL.String())
-	fi, err := os.Stat(file_path)
-	if err != nil {
-		// 404
-		fmt.Fprintln(w, "Not found: ", file_path)
-		return
-	}
-	if fi.IsDir() {
-		w.Header().Add("Content-Type", "text/html; charset=UTF-8")
-		fmt.Fprintln(w, "<!DOCTYPE html> <html><body>")
-		// 先写特殊目录 ..
-		fmt.Fprintf(w, "<p><strong> <a href=%s/>%s</a> </strong></p>\n", "..", "..")
-		c, err := ioutil.ReadDir(file_path)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "ReadDir error")
-		}
-		for _, entry := range c {
-			if entry.IsDir() {
-				// 目录必须以/结尾,切记
-				fmt.Fprintf(w, "<strong> <a href=%s/>%s</a> </strong> <br>\n", entry.Name(), entry.Name())
-			} else {
-				fmt.Fprintf(w, "<a href=%s>%s</a> <br>\n", entry.Name(), entry.Name())
-			}
-		}
-		fmt.Fprintln(w, "</body></html>")
-		return
-	}
-	file, err := os.ReadFile(file_path)
-	w.Write(file)
-	return
-}
 
 func main() {
 	rootPtr := flag.String("root", ".", "root dir to serve")
@@ -61,7 +23,7 @@ func main() {
 
 	flag.Parse()
 
-	indexing_path = *rootPtr
+	var indexing_path = *rootPtr
 	var err error
 	_, err = os.Stat(indexing_path)
 	if err != nil {
@@ -69,7 +31,10 @@ func main() {
 	}
 
 	address := fmt.Sprintf("%s:%d", *addressPtr, *portPtr)
+	fileServer := http.FileServer(http.Dir(indexing_path))
+	http.Handle("/", fileServer)
 	fmt.Printf("listening on %s, dir %s", address, indexing_path)
-	http.HandleFunc("/", index)
-	http.ListenAndServe(address, nil)
+	if err := http.ListenAndServe(address, nil); err != nil{
+		panic(err)
+	}
 }
